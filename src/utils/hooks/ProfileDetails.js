7c6b2539fe12d195edc99bd7166e6c0199e1ded8
Sandeep <sandeepkushaj3@gmail.com>
2021-01-14 02:59:06 +0530

    profile pic bu fixed

--- src/utils/hooks/ProfileDetails.js
+++ src/utils/hooks/ProfileDetails.js
@@ -13,47 +13,47 @@ function ProfileDetails(usDetails) {
         if(localStorage.getItem("usValues") !== null){
             if(Object.keys(JSON.parse(localStorage.getItem("usValues"))).length !==0){
                 const userDetails = JSON.parse(localStorage.getItem("usValues"));
-                if(usDetails) {
-                    if (usDetails.is_teacher) {
-                        await Axios.get(`${process.env.REACT_APP_LMS_MAIN_URL}/account-api/profile/${userDetails.user.id}/`, {
-                            headers: {Authorization: 'Token ' + userDetails.key}
-                        }).then(res => {
-                            setprofileDetails({
-                                name: res.data.user.first_name,
-                                lname: res.data.user.last_name,
-                                phoneNumber: res.data.user.phone_no,
-                                email: res.data.user.email,
-                                userName: res.data.user.username,
-                                address: res.data.user.address,
-                                pic:res.data.profile_pic
-                            });
-                        }).catch(err => {
-                            if (err) {
-                                hadelLogOut();
-                            }
-                        })
-                    } else {
-                        await Axios.get(`${process.env.REACT_APP_LMS_MAIN_URL}/account-api/stuprofile/${userDetails.user.id}/`, {
-                            headers: {Authorization: 'Token ' + userDetails.key}
-                        }).then(res => {
-                            setprofileDetails({
-                                name: res.data.user.first_name,
-                                lname: res.data.user.last_name,
-                                phoneNumber: res.data.user.phone_no,
-                                email: res.data.user.email,
-                                userName: res.data.user.username,
-                                address: res.data.user.address,
-                                pic:res.data.profile_pic
-                            });
-                        }).catch(err => {
-                            if (err) {
-                                hadelLogOut();
-                            }
-                        })
-                    }
+                console.log(userDetails)
+                if (userDetails.user.is_teacher) {
+                    await Axios.get(`${process.env.REACT_APP_LMS_MAIN_URL}/account-api/profile/${userDetails.user.id}/`, {
+                        headers: {Authorization: 'Token ' + userDetails.key}
+                    }).then(res => {
+                        setprofileDetails({
+                            name: res.data.user.first_name,
+                            lname: res.data.user.last_name,
+                            phoneNumber: res.data.user.phone_no,
+                            email: res.data.user.email,
+                            userName: res.data.user.username,
+                            address: res.data.user.address,
+                            pic:res.data.profile_pic
+                        });
+                    }).catch(err => {
+                        if (err) {
+                            hadelLogOut();
+                        }
+                    })
+                } else {
+                    await Axios.get(`${process.env.REACT_APP_LMS_MAIN_URL}/account-api/stuprofile/${userDetails.user.id}/`, {
+                        headers: {Authorization: 'Token ' + userDetails.key}
+                    }).then(res => {
+                        setprofileDetails({
+                            name: res.data.user.first_name,
+                            lname: res.data.user.last_name,
+                            phoneNumber: res.data.user.phone_no,
+                            email: res.data.user.email,
+                            userName: res.data.user.username,
+                            address: res.data.user.address,
+                            pic:res.data.profile_pic
+                        });
+                    }).catch(err => {
+                        if (err) {
+                            hadelLogOut();
+                        }
+                    })
                 }
             }
         }
+
     },[])
     return profileDetails;
 }
