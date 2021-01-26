aa0663a1c73c2579cb779a29294728c1e1aa2ed5
ondev97 <68001041+ondev97@users.noreply.github.com>
2021-01-26 18:57:32 +0530

    Removed login check

--- src/utils/hooks/StValidateLogin.js
+++ src/utils/hooks/StValidateLogin.js
@@ -33,13 +33,30 @@ function StValidateLogin() {
     }
 
     function submit() {
-        console.log();
-        Axios.post(`${process.env.REACT_APP_LMS_MAIN_URL}/account-api/testlogin/`,{
-            username:values.email,
-            password:values.pw
-        }).then(res=>{
-            if(!res.data.status) {
-                Axios.post(`${process.env.REACT_APP_LMS_MAIN_URL}/rest-auth/login/`, {
+//         console.log();
+//         Axios.post(`${process.env.REACT_APP_LMS_MAIN_URL}/account-api/testlogin/`,{
+//             username:values.email,
+//             password:values.pw
+//         }).then(res=>{
+//             if(!res.data.status) {
+//                 Axios.post(`${process.env.REACT_APP_LMS_MAIN_URL}/rest-auth/login/`, {
+//                     username: values.email,
+//                     password: values.pw
+//                 }).then(res => {
+//                     setacDetails(res.data);
+//                 }).catch(err => {
+//                     seterrors({...errors, "comerrors": err.response.data.non_field_errors});
+//                 });
+//             }else{
+//                 seterrors({...errors, "comerrors": 'Someone is already logged into this account'});
+//             }
+//         }).catch(err=>{
+//             //check err and set state
+//             // console.log(err.response.data);
+//             // seterrors({...errors,"comerrors":err.response.data});
+//         })
+        
+        Axios.post(`${process.env.REACT_APP_LMS_MAIN_URL}/rest-auth/login/`, {
                     username: values.email,
                     password: values.pw
                 }).then(res => {
@@ -47,14 +64,6 @@ function StValidateLogin() {
                 }).catch(err => {
                     seterrors({...errors, "comerrors": err.response.data.non_field_errors});
                 });
-            }else{
-                seterrors({...errors, "comerrors": 'Someone is already logged into this account'});
-            }
-        }).catch(err=>{
-            //check err and set state
-            // console.log(err.response.data);
-            // seterrors({...errors,"comerrors":err.response.data});
-        })
 
     }
 
