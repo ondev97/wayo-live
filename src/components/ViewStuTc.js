5e5e37bdfcdfce0052aecaf77bbba93f4a4ecf45
Osada97 <osadamanohara55@gmail.com>
2021-02-14 02:05:04 +0530

    Added Api and added infinite scrolla for table

--- src/components/ViewStuTc.js
+++ src/components/ViewStuTc.js
@@ -2,11 +2,12 @@ import React from 'react';
 import child1 from '../img/child.png';
 import '../assets/css/viewst.css';
 
-export default function ViewStuTc({setmodelOp,modelOp}) {
+export default function ViewStuTc({setmodelOp,modelOp,stPrDetail,setstPrDetail}) {
 
     const close = (e) =>{
         if(e.target.classList.contains('modelbg') || e.target.classList.contains('vlvl')){
             setmodelOp(false);
+            setstPrDetail([]);
         }
     }
 
@@ -14,6 +15,7 @@ export default function ViewStuTc({setmodelOp,modelOp}) {
         <div>
             {
                 modelOp ? (
+                    stPrDetail.length !== 0 ? (
                         <div className="modelbg" onClick={close}>
                             <div className="modelcard">
                                 <div className="clsst">
@@ -25,17 +27,19 @@ export default function ViewStuTc({setmodelOp,modelOp}) {
                                     </div>
                                 </div>
                                 <div className="pagebottom">
-                                    <h1>Osada Manohara Rathnayake</h1>
-                                    <h2>Ozka</h2>
-                                    <h3>076-859790</h3>
-                                    <h3>osadamanohara55@gmail.com</h3>
-                                    <h4>Chirathma Furniture,kandegedara road,haliela</h4>
+                                    <h1>{stPrDetail.user.first_name+" "+stPrDetail.user.last_name}</h1>
+                                    <h2>{stPrDetail.user.username}</h2>
+                                    <h3>{stPrDetail.user.email}</h3>
+                                    <h3>{stPrDetail.user.phone_no}</h3>
+                                    <h4>{stPrDetail.user.address}</h4>
                                     <div className="dis">
-                                        <p>kaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane rkaksnd kask dka kas asdn jkas maksn kjas ndkasdkaj nk nasdnjaks dnka saksnjk n soiane r</p>
+                                        <p>{stPrDetail.description}</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                    ):""
+                        
                 ) :""
             }
         </div>
--- src/pages/AllStList.js
+++ src/pages/AllStList.js
@@ -6,44 +6,95 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { useHistory, useParams } from 'react-router-dom';
 import Empty from '../components/Empty';
 import ViewStuTc from '../components/ViewStuTc';
+import { store } from 'react-notifications-component';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default function AllStList() {
 
      //get acDetails from Redux Store
     const usDetails = useSelector(state => state.accountDetails);
     const [allstudent, setallstudent] = useState([]);
+    const [allDetailSt, setallDetailSt] = useState(null);
     const [modelOp, setmodelOp] = useState(false);
+    const [unEnrol, setunEnrol] = useState(false);
+    const [stPrDetail, setstPrDetail] = useState([]);
+    const [page, setpage] = useState(1);
     const history = useHistory();
     const {cid} = useParams();
 
     const getallStude = async () =>{
         if(usDetails){
-            await Axios.get(`${process.env.REACT_APP_LMS_MAIN_URL}/course-api/students/${cid}/`,{
+            await Axios.get(`${process.env.REACT_APP_LMS_MAIN_URL}/course-api/students/${cid}/?page=${page}`,{
                 headers:{Authorization:'Token '+usDetails.key}
             }).then(res=>{
-                console.log(res.data);
-                setallstudent([...res.data])
+                setallDetailSt(res.data);
+                setallstudent([...allstudent,...res.data.results])
             })
         }
     }
 
     useEffect(() => {
         getallStude();
-    }, [usDetails]);
-
+    }, [usDetails,unEnrol,page]);
+    /*model page*/
     const back = () =>{
         history.goBack();
     }
-
-    const viewPr = () =>{
+    /*model page*/
+    const viewPr = (id) =>{
         if(!modelOp){
             setmodelOp(true);
+            /*get studet details */
+            Axios.get(`${process.env.REACT_APP_LMS_MAIN_URL}/account-api/stuprofile/${id}/`,{
+                headers:{Authorization:'Token '+usDetails.key}
+            }).then(res=>{
+                setstPrDetail(res.data);
+            }).catch(err=>{
+                console.log(err);
+            })
         }
     }
 
+    /*Student Unenrolled*/
+    const unEnrolSt = async (id)=>{
+        if(window.confirm("Are You Sure?")){
+            await Axios.delete(`${process.env.REACT_APP_LMS_MAIN_URL}/course-api/unenroll/${cid}/${id}/`,{
+                headers:{Authorization:'Token ' + usDetails.key}
+            }).then(()=>{
+                setunEnrol(!unEnrol);
+                store.addNotification({
+                    title: `Unenrolled Successfully`,
+                    message: "Eyekon eClass",
+                    type: "success",
+                    insert: "top",
+                    container: "top-right",
+                    animationIn: ["animate__animated", "animate__fadeIn"],
+                    animationOut: ["animate__animated", "animate__fadeOut"],
+                    dismiss: {
+                      duration: 2000,
+                      onScreen: true,
+                      pauseOnHover: true,
+                      showIcon:true
+                    },
+                    width:400
+                });
+                
+            }).catch(err=>{
+                console.log(err);
+            })
+        }
+    }
+    function next(){
+        if(allDetailSt.next){
+            console.log(page);
+            setpage(page+1);
+        };
+    }
+
+
     return (
         <div className="stlist">
-            <ViewStuTc setmodelOp={setmodelOp} modelOp={modelOp}/>
+            <ViewStuTc setmodelOp={setmodelOp} modelOp={modelOp} stPrDetail={stPrDetail} setstPrDetail={setstPrDetail}/>
             <div className="pageTop">
                 <h1>Enrolled Students</h1>
             </div>
@@ -54,10 +105,14 @@ export default function AllStList() {
                     <button><i className="fas fa-search"></i></button>
                 </div>
             </div>
+            {
+                allDetailSt !== null ?
             <div className="sttable">
                 {
                     allstudent ? 
-                        (<table>
+                        (
+                            <InfiniteScroll dataLength={allDetailSt.count} next={next} hasMore={true}>
+                            <table>
                             <thead>
                                 <tr>
                                     <th>Id</th>
@@ -71,22 +126,28 @@ export default function AllStList() {
                             <tbody>
                                 {
                                     allstudent.map((data)=>(
-                                            <tr key={data.id} onClick={viewPr}>
-                                                <td>{data.id}</td>
-                                                <td><LazyLoadImage src={data.profile_pic} effect="blur"/></td>
-                                                <td>{data.user.first_name+" "+data.user.last_name}</td>
-                                                <td>{data.user.username}</td>
-                                                <td>{data.user.email}</td>
-                                                <td><button><i className="far fa-times-circle"></i></button></td>
+                                        <tr key={data.id}>
+                                                <td onClick={()=>viewPr(data.user.id)}>{data.id}</td>
+                                                <td onClick={()=>viewPr(data.user.id)}><LazyLoadImage src={data.profile_pic} effect="blur"/></td>
+                                                <td onClick={()=>viewPr(data.user.id)}>{data.user.first_name+" "+data.user.last_name}</td>
+                                                <td onClick={()=>viewPr(data.user.id)}>{data.user.username}</td>
+                                                <td onClick={()=>viewPr(data.user.id)}>{data.user.email}</td>
+                                                <td>
+                                                    <button onClick={()=>unEnrolSt(data.id)}><i className="far fa-times-circle"></i></button>
+                                                </td>
                                             </tr>
                                     ))
                                     
                                 }
                             </tbody>
-                        </table>)
-                    : <Empty target="No Students"/>
-                }
+                        </table>
+                        </InfiniteScroll> 
+                        )
+                        : <Empty target="No Students"/>
+                    }
             </div>
+                    : ''
+            }
 
         </div>
     )
