4871637a584ef257b8b8d905d3682a4f267823d8
Osada97 <osadamanohara55@gmail.com>
2021-03-05 00:00:00 +0530

    Changed signup page styles

--- src/pages/StSignUp.js
+++ src/pages/StSignUp.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import StSignupForm from '../components/StSignupForm';
 import "../assets/css/stsignup.css";
-import signupsvg from "../img/svg/signup.svg";
 import { useDispatch } from 'react-redux';
 import { activeAccount } from '../actions';
+import logo from '../img/Logo_1.png';
 import RegisterInfo from '../components/RegisterInfo';
 
 export default function StSignUp() {
@@ -15,14 +15,19 @@ export default function StSignUp() {
     return (
         <>
             <div className="siup_body">
-            <RegisterInfo/>
                 <div className="siup_column">
                     <div className="siup_form">
+                        <div className="lgsi">
+                            <img src={logo} alt=""/>
+                        </div>
                         <div className="topSign">
                             <h2>Student Registration</h2>
                         </div>
                         <StSignupForm/>
                     </div>
+                </div>
+                <div className="siup_column">
+                    <RegisterInfo/>
                 </div>
             </div>
         </>
