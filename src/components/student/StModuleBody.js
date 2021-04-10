8ba3eeb3062bb26d1e4b88446b3571414466cea3
Osada97 <osadamanohara55@gmail.com>
2021-04-10 14:39:03 +0530

    Added off module

--- src/components/student/StModuleBody.js
+++ src/components/student/StModuleBody.js
@@ -1,20 +1,24 @@
-import { motion } from 'framer-motion';
-import React, { useState } from 'react'
+import { motion } from "framer-motion";
+import React, { useState } from "react";
 
-export default function StModuleBody({children,name}) {
-    const [isToggle, setisToggle] = useState(true);
+export default function StModuleBody({ children, name }) {
+  const [isToggle, setisToggle] = useState(false);
 
-    return (
-        <motion.div layout className="al_on_model">
-            <motion.div layout className="on_model_head">
-                <h1>{name}</h1>
-                <div className="heads_buts">
-                    <button onClick={()=>setisToggle(!isToggle)}><i className="fas fa-chevron-down"></i></button>
-                </div>
-            </motion.div>
-            {
-                isToggle ? children : ''
-            }
-        </motion.div>
-    )
+  return (
+    <motion.div layout className="al_on_model">
+      <motion.div
+        layout
+        className="on_model_head"
+        onClick={() => setisToggle(!isToggle)}
+      >
+        <h1>{name}</h1>
+        <div className="heads_buts">
+          <button onClick={() => setisToggle(!isToggle)}>
+            <i className="fas fa-chevron-down"></i>
+          </button>
+        </div>
+      </motion.div>
+      {isToggle ? children : ""}
+    </motion.div>
+  );
 }
