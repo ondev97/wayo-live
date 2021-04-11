a96235d0c20a3208c3846f27110564a33533b86a
Osada97 <osadamanohara55@gmail.com>
2021-04-11 20:12:42 +0530

    Added Lazyload for teacher side

--- src/components/TcOneModel.js
+++ src/components/TcOneModel.js
@@ -1,115 +1,165 @@
-import Axios from 'axios';
-import { AnimateSharedLayout } from 'framer-motion';
-import React from 'react';
-import ReactHtmlParser  from 'react-html-parser';
-import ReactPlayer from 'react-player/lazy';
-import { useSelector } from 'react-redux';
-import ModuleBody from './ModuleBody';
+import Axios from "axios";
+import { AnimateSharedLayout } from "framer-motion";
+import React from "react";
+import ReactHtmlParser from "react-html-parser";
+import LazyLoad from "react-lazyload";
+import ReactPlayer from "react-player/lazy";
+import { useSelector } from "react-redux";
+import ModuleBody from "./ModuleBody";
 
-export default function TcOneModel({name,msg,moduleFiles,id,cid,setisRemoveModule}) {
+export default function TcOneModel({
+  name,
+  msg,
+  moduleFiles,
+  id,
+  cid,
+  setisRemoveModule,
+}) {
+  //get acDetails from Redux Store
+  const usDetails = useSelector((state) => state.accountDetails);
 
-    //get acDetails from Redux Store
-    const usDetails = useSelector(state => state.accountDetails);
-
-    //filtering message and embed react player
-    function filterTags(nodes){
-        let media = [];
-        if(nodes.length > 0){
-            for(let i=0;i<nodes.length;i++){
-                if(nodes[i].type === 'figure' && nodes[i].props.className === 'media'){
-                    if(nodes[i].props.children){
-                        for(let x=0;x<nodes[i].props.children.length;x++){
-                            if(nodes[i].props.children[x].type ==='oembed'){
-                                media.push(
-                                    <div className='re_player' key={i}>
-                                        <ReactPlayer url={nodes[i].props.children[x].props.url} controls={true} pip={true} className='player' width='100%' height='100%'/>
-                                    </div>
-                                    )
-                            }
-                            else{
-                                media = [...media,nodes[i]];
-                            }
-                        }
-                    }
-                    else{
-                        media = [...media,nodes[i]];
-                    }
-                }
-                else{
-                    media = [...media,nodes[i]];
-                }
+  //filtering message and embed react player
+  function filterTags(nodes) {
+    let media = [];
+    if (nodes.length > 0) {
+      for (let i = 0; i < nodes.length; i++) {
+        if (
+          nodes[i].type === "figure" &&
+          nodes[i].props.className === "media"
+        ) {
+          if (nodes[i].props.children) {
+            for (let x = 0; x < nodes[i].props.children.length; x++) {
+              if (nodes[i].props.children[x].type === "oembed") {
+                media.push(
+                  <div className="re_player" key={i}>
+                    <ReactPlayer
+                      url={nodes[i].props.children[x].props.url}
+                      controls={true}
+                      pip={true}
+                      className="player"
+                      width="100%"
+                      height="100%"
+                    />
+                  </div>
+                );
+              } else {
+                media = [...media, nodes[i]];
+              }
             }
-            return media;
+          } else {
+            media = [...media, nodes[i]];
+          }
+        } else {
+          media = [...media, nodes[i]];
         }
+      }
+      return media;
     }
+  }
 
-    const functionRemoveModule = (id) =>{
-
-        Axios.delete(`${process.env.REACT_APP_LMS_MAIN_URL}/course-api/deletemodule/${id}/`,{
-            headers:{Authorization:"Token " + usDetails.key}
-        }).then(()=>{
-            setisRemoveModule(true);
-        }).catch(err=>{
-
-        })
-
-    }
-
-    const handelDeleteModule = async (id) =>{
-        if(window.confirm('Are You Sure?')){
-            setisRemoveModule(false);
-            await functionRemoveModule(id);
-        }
-    }
-    
-    return (
-            <AnimateSharedLayout>
-            <ModuleBody name={name} id={id} cid={cid} handelDeleteModule={handelDeleteModule}>
-                <div className="on_model_body">
-                    {
-                        msg &&
-                            <div className="model_body_row">
-                                {filterTags(ReactHtmlParser(msg))}
-                            </div>
-                    }
-                    {
-                        moduleFiles.length !== 0 ? 
-
-                            moduleFiles.map((files)=>(
-                                parseInt(Object.keys(files)) === id &&
-                                Object.values(files)[0].map(fl =>(
-                                    <div className="model_body_row" key={fl.id}>
-                                        <p>
-                                            {
-                                                fl.file.substring(fl.file.lastIndexOf('.')+1) === 'jpg' || fl.file.substring(fl.file.lastIndexOf('.')+1) === 'png' || fl.file.substring(fl.file.lastIndexOf('.')+1) === 'jpeg' ?
-                                                            <i className="fas fa-file-image"></i>
-                                                : fl.file.substring(fl.file.lastIndexOf('.')+1) === 'pdf' ?
-                                                            <i className="fas fa-file-pdf"></i>
-                                                : fl.file.substring(fl.file.lastIndexOf('.')+1) === 'pptx' || fl.file.substring(fl.file.lastIndexOf('.')+1) === 'pptm' || fl.file.substring(fl.file.lastIndexOf('.')+1) === 'ppt' ?
-                                                            <i className="fas fa-file-powerpoint"></i>
-                                                : fl.file.substring(fl.file.lastIndexOf('.')+1) === 'mp4' || fl.file.substring(fl.file.lastIndexOf('.')+1) === 'mkv' ?
-                                                            <i className="fab fa-youtube"></i>
-                                                : fl.file.substring(fl.file.lastIndexOf('.')+1) === 'mp3' ?
-                                                            <i className="far fa-file-audio"></i>
-                                                : fl.file.substring(fl.file.lastIndexOf('.')+1) === 'xlsx' || fl.file.substring(fl.file.lastIndexOf('.')+1) === 'xlsb' ?
-                                                        <i className="far fa-file-excel"></i>
-                                                : fl.file.substring(fl.file.lastIndexOf('.')+1) === 'html' ?
-                                                        <i className="fab fa-html5"></i>
-                                                : fl.file.substring(fl.file.lastIndexOf('.')+1) === 'css'?
-                                                        <i className="fab fa-css3-alt"></i>
-                                                : fl.file.substring(fl.file.lastIndexOf('.')+1) === 'js' ?    
-                                                        <i className="fab fa-js-square"></i>
-                                                : <i className="far fa-file-alt"></i>
-                                            } 
-                                            <a href={`${fl.file}`} target="_blank" download>{fl.file_name || fl.id}</a> </p>
-                                    </div>
-                                ))
-                            ))
-
-                        : ''
-                    }
-                </div>
-            </ModuleBody>
-            </AnimateSharedLayout>
+  const functionRemoveModule = (id) => {
+    Axios.delete(
+      `${process.env.REACT_APP_LMS_MAIN_URL}/course-api/deletemodule/${id}/`,
+      {
+        headers: { Authorization: "Token " + usDetails.key },
+      }
     )
+      .then(() => {
+        setisRemoveModule(true);
+      })
+      .catch((err) => {});
+  };
+
+  const handelDeleteModule = async (id) => {
+    if (window.confirm("Are You Sure?")) {
+      setisRemoveModule(false);
+      await functionRemoveModule(id);
+    }
+  };
+
+  return (
+    <AnimateSharedLayout>
+      <LazyLoad height={200}>
+        <ModuleBody
+          name={name}
+          id={id}
+          cid={cid}
+          handelDeleteModule={handelDeleteModule}
+        >
+          <div className="on_model_body">
+            {msg && (
+              <div className="model_body_row">
+                {filterTags(ReactHtmlParser(msg))}
+              </div>
+            )}
+            {moduleFiles.length !== 0
+              ? moduleFiles.map(
+                  (files) =>
+                    parseInt(Object.keys(files)) === id &&
+                    Object.values(files)[0].map((fl) => (
+                      <div className="model_body_row" key={fl.id}>
+                        <p>
+                          {fl.file.substring(fl.file.lastIndexOf(".") + 1) ===
+                            "jpg" ||
+                          fl.file.substring(fl.file.lastIndexOf(".") + 1) ===
+                            "png" ||
+                          fl.file.substring(fl.file.lastIndexOf(".") + 1) ===
+                            "jpeg" ? (
+                            <i className="fas fa-file-image"></i>
+                          ) : fl.file.substring(
+                              fl.file.lastIndexOf(".") + 1
+                            ) === "pdf" ? (
+                            <i className="fas fa-file-pdf"></i>
+                          ) : fl.file.substring(
+                              fl.file.lastIndexOf(".") + 1
+                            ) === "pptx" ||
+                            fl.file.substring(fl.file.lastIndexOf(".") + 1) ===
+                              "pptm" ||
+                            fl.file.substring(fl.file.lastIndexOf(".") + 1) ===
+                              "ppt" ? (
+                            <i className="fas fa-file-powerpoint"></i>
+                          ) : fl.file.substring(
+                              fl.file.lastIndexOf(".") + 1
+                            ) === "mp4" ||
+                            fl.file.substring(fl.file.lastIndexOf(".") + 1) ===
+                              "mkv" ? (
+                            <i className="fab fa-youtube"></i>
+                          ) : fl.file.substring(
+                              fl.file.lastIndexOf(".") + 1
+                            ) === "mp3" ? (
+                            <i className="far fa-file-audio"></i>
+                          ) : fl.file.substring(
+                              fl.file.lastIndexOf(".") + 1
+                            ) === "xlsx" ||
+                            fl.file.substring(fl.file.lastIndexOf(".") + 1) ===
+                              "xlsb" ? (
+                            <i className="far fa-file-excel"></i>
+                          ) : fl.file.substring(
+                              fl.file.lastIndexOf(".") + 1
+                            ) === "html" ? (
+                            <i className="fab fa-html5"></i>
+                          ) : fl.file.substring(
+                              fl.file.lastIndexOf(".") + 1
+                            ) === "css" ? (
+                            <i className="fab fa-css3-alt"></i>
+                          ) : fl.file.substring(
+                              fl.file.lastIndexOf(".") + 1
+                            ) === "js" ? (
+                            <i className="fab fa-js-square"></i>
+                          ) : (
+                            <i className="far fa-file-alt"></i>
+                          )}
+                          <a href={`${fl.file}`} target="_blank" download>
+                            {fl.file_name || fl.id}
+                          </a>{" "}
+                        </p>
+                      </div>
+                    ))
+                )
+              : ""}
+          </div>
+        </ModuleBody>
+      </LazyLoad>
+    </AnimateSharedLayout>
+  );
 }
